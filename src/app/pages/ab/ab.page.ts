dfac42e20b282afdc16961ce53d6042a9b657742
DarkestTimeline <sfvasquez96@gmail.com>
2022-07-14 20:02:21 -0600

    Mejoras a AB

--- src/app/pages/ab/ab.page.ts
+++ src/app/pages/ab/ab.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ab',
@@ -6,22 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ab.page.scss'],
 })
 export class AbPage implements OnInit {
-  isVisible2 = false;
-  isVisible3 = false;
+  isVisible21 = false;
+  isVisible22 = false;
 
-  constructor() { }
+  constructor(public alertCTRL: AlertController) { }
 
   ngOnInit() {
   }
 
   onClickMostrar2(){
-    this.isVisible2 = true;
-    this.isVisible3 = false;
+    this.isVisible21 = true;
+    this.isVisible22 = false;
   }
 
   onClickMostrar3(){
-    this.isVisible3 = true;
-    this.isVisible2 = false;
+    this.isVisible22 = true;
+    this.isVisible21 = false;
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertCTRL.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirmacion',
+      message: 'Has resuelto tu problema?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Si',
+          id: 'confirm-button',
+          handler: () => {
+            window.location.href = '/principal';
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
